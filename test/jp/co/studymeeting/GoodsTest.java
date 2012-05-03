d331a12b256a8bb623ac6bbdae20513065352d9d
Ikuo Suyama <martin-d28jp-love@i.softbank.jp>
2012-05-03 10:52:27 +0900

    コンパイルエラー

--- test/jp/co/studymeeting/GoodsTest.java
+++ test/jp/co/studymeeting/GoodsTest.java
@@ -3,5 +3,7 @@ package jp.co.studymeeting;
 import junit.framework.TestCase;
 
 public class GoodsTest extends TestCase {
-
+    public void testGetPrice() {
+        assertEquals(books .getPrice(), 2000);
+    }
 }
