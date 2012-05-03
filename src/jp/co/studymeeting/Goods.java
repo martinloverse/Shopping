726b2167b1cdff2fbfb076d7329f50ba313ab949
Ikuo Suyama <martin-d28jp-love@i.softbank.jp>
2012-05-03 13:39:12 +0900

    テストグリーン

--- src/jp/co/studymeeting/Goods.java
+++ src/jp/co/studymeeting/Goods.java
@@ -2,9 +2,8 @@ package jp.co.studymeeting;
 
 public class Goods {
 
-    public Object getPrice() {
-        // TODO 自動生成されたメソッド・スタブ
-        return null;
+    public int getPrice() {
+        return 2000;
     }
 
 }
