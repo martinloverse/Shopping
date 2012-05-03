ad80ea52d47e06c3f7d21d451c9ffdd6a1a1666e
Ikuo Suyama <martin-d28jp-love@i.softbank.jp>
2012-05-03 10:54:39 +0900

    インスタンス生成

--- test/jp/co/studymeeting/GoodsTest.java
+++ test/jp/co/studymeeting/GoodsTest.java
@@ -4,6 +4,7 @@ import junit.framework.TestCase;
 
 public class GoodsTest extends TestCase {
     public void testGetPrice() {
+        Goods books = new Goods();
         assertEquals(books .getPrice(), 2000);
     }
 }
